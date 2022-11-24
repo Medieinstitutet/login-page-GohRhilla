339c435edb6c1225b6542b474643d5f3afa0c8f8
Daniel <88243933+GohRhilla@users.noreply.github.com>
2022-11-24 16:33:25 +0100

    rejäla omtag. Slimmat och skrivit om funktioner.

--- script.js
+++ script.js
@@ -1,83 +1,74 @@
-let userID = document.getElementById("usernameBox");
-let userPassword = document.getElementById("passwordBox");
-let loggedOut = document.getElementById("loggedOut");
-
-let welcomeMsg = document.getElementById("welcome");
-let notification = document.getElementById("status");
 let header = document.getElementById("header");
 let footer = document.getElementById("footer");
+let mainBox = document.getElementById("mainBox")
 
-let loginBtn = document.getElementById("loginButton");
-let logOutBtn = document.getElementById("logOutButton");
+let userID = document.getElementById("usernameBox");
+let userPassword = document.getElementById("passwordBox");
+let placeholderBtn = document.getElementById("placeholderBtn");
 
 let userData = [
     {userName: "janne", password: "test"}
 ];
 
-if (localStorage.getItem("userName")) {            
+if (localStorage.getItem("userName")) {
     loggedIn();
-    // loggedInStyle();
-} else {
-    printNotLoggedIn();
+    toggleBtn();
+} else {    
+    createLoginBtn();
 } 
 
-loginBtn.addEventListener("click", () => {
-    let loginUser = userID.value;
-    localStorage.setItem("userName", loginUser);
-    let loginPassword = userPassword.value;
-    localStorage.setItem("userPassword", loginPassword);
-    
-    checkPassword();
-})
-
 function checkPassword() {     
-    console.log('klick på knapp ');      
+    console.log('klick på knapp');      
     for (i = 0; i < userData.length; i++) {
         if (userID.value == userData[i].userName && userPassword.value == userData[i].password) {
-        hej.innerHTML = "Du är inloggad som: " + userID.value;
-        return true;
+            loggedIn();            
+
+            let loginUser = userID.value
+            localStorage.setItem("userName", loginUser);
+            toggleBtn();        
+
+            userID.value="";
+            userPassword.value="";
+             
+        } else {
+            alert("Wrong username or password");
+            userID.value="";
+            userPassword.value="";
         }
     }
-    hej.innerHTML = "Användaren finns inte";    
 }
 
-function loggedIn() {
-    let userName = localStorage.getItem("userName");
-    loggedOut.innerText = "\nDu är inloggad som: " +userName+"\n"+"\n";
+function toggleBtn () {
+    placeholderBtn.innerHTML = "";
 
-    let logOutBtn = document.createElement('button');
-    logOutBtn.innerText = "Log out";        
-    logOutBtn.addEventListener("click", () => {
-        localStorage.removeItem("userName", "userPassword");
-        printNotLoggedIn
+    let logoutBtn = document.createElement("button");
+    placeholderBtn.appendChild(logoutBtn);
+    logoutBtn.innerText = "logout";
+
+    logoutBtn.addEventListener("click", () => {
+        alert("Du har loggats ut");
+        localStorage.removeItem("userName");
+        createLoginBtn();
+    })      
+}
+
+function createLoginBtn () {
+    placeholderBtn.innerHTML ="";
+
+    let loginBtn = document.createElement("button");
+    placeholderBtn.appendChild(loginBtn);
+    loginBtn.innerText = "Login";
+
+    loginBtn.addEventListener("click", () => {
+        checkPassword();        
     })
-    loggedOut.appendChild(logOutBtn);
-    loggedInStyle();
 }
 
-function loggedInStyle() {
-    userID.style.display="none";
-    userPassword.style.display="none";
-    loginBtn.style.display="none";
+function loggedIn() {    
+    alert("inloggad");
+    mainBox.style.visibility ="initial";
 }
 
-function printNotLoggedIn() {
-    console.log("inte inloggad");
+function loggedout() {
+    mainBox.style.visibility = "hidden";
 }
-
-/*To do
-* skapa en ett dolt meddalande som endast syns vid felaktigt lösenord
-- Alert?
-* Skapa funktioner: klick på login
-* Skapa localStorage
-* Om användaren redan är inloggad... if/function? användare inloggad... bla bla
-* Från localStorage, om användaren är inloggad visa välkommen "namn" 
-- annars login-ruta.
-* Ändra så att lösenord inte syns
-*/
-
-// welcomeMsg.innerHTML=""; - tömmer innehållet
-// annars kanske det bara går att skriva över...
-
-//if logged in ändra färg på rutan?
-
